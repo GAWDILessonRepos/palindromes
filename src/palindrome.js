ab98d93334dc0f82bfed47e1ef50753f686e639a
Doug <djmacdonald@uchicago.edu>
2015-03-26 09:23:19 -0400

    palindrom count

--- src/palindrome.js
+++ src/palindrome.js
@@ -1,1 +1,14 @@
 var palindrome = {};
+
+palindrome.count = function(arr){
+  pali = [];
+  arr.map(function(phrase){
+    var a = phrase.split(' ').join('').toLowerCase();
+    if (a === a.split('').reverse().join('')){
+      pali.push(a);
+    }
+  });
+  return pali.length;
+};
+
+// palindrome.count = function(){};
