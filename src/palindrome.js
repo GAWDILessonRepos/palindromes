8b09215d112000709b1cdbffbb52cf8b993aa733
Doug <djmacdonald@uchicago.edu>
2015-03-26 09:26:00 -0400

    used js filter. result is cleaner

--- src/palindrome.js
+++ src/palindrome.js
@@ -1,14 +1,10 @@
 var palindrome = {};
 
 palindrome.count = function(arr){
-  pali = [];
-  arr.map(function(phrase){
-    var a = phrase.split(' ').join('').toLowerCase();
-    if (a === a.split('').reverse().join('')){
-      pali.push(a);
-    }
-  });
-  return pali.length;
+  return arr.filter(isPali).length;
 };
 
-// palindrome.count = function(){};
+isPali = function(phrase){
+  var a = phrase.split(' ').join('').toLowerCase();
+  return a === a.split('').reverse().join('');
+};
